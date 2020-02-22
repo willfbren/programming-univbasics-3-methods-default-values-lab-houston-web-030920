c720e9fd8d1e445b1ac8e0f067694e2417451bc7
Will Brennan <willfbren@gmail.com>
2020-02-22 18:24:54 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,7 +4,7 @@
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  meal_choice("broccoli", "macaroni")
+  return meal_choice("broccoli", "macaroni")
 end
 
 
